c4f176f8cb4fa0c2435de08a2aa302a9dce0f97b
ayrapetyansergei <ayrapetyan.96@yandex.ru>
2021-07-18 01:47:03 +0300

    upload file with form

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+from flask import Flask, request, render_template, redirect, url_for
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
@@ -8,6 +11,21 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
 	return 'First commit'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		filename=secure_filename(f.filename)
+		f.save(os.path.join('/home/serj/Desktop/file-hosting/storage', filename))
+		return redirect(url_for('save_file', filename=filename))
+	else:
+		return render_template('upload_file.html')
+
+@app.route('/uploads/<filename>')
+def save_file(filename):
+	return send_from_directory('/home/serj/Desktop/file-hosting/storage', filename)
+
 
 if __name__ == '__main__':
 	app.run()
