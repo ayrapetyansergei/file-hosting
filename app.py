d0a7407f40f98b9795edeb1fa4009170c1e465b6
ayrapetyansergei <ayrapetyan.96@yandex.ru>
2021-07-19 01:21:04 +0300

    add list file and datetime in mainpage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, request, render_template, redirect, url_for
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
@@ -6,11 +7,18 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+metadata = list()
 
-
-@app.route('/')
-def index():
-	return 'First commit'
+@app.route('/', methods=['GET', 'POST'])
+def index(filename=None, date_time=None):
+	global metadata
+	if request.method == 'POST':
+		filename = request.args.get('filename', None)
+		date_time = request.args.get('date_time', None)
+		if not(filename is None) and not(date_time is None):
+			metadata.append({'filename':filename, 'date_time':date_time})
+		return redirect('/upload')
+	return render_template('index.html', metadata=metadata)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -18,7 +26,7 @@ def upload_file():
 		f = request.files['file']
 		filename=secure_filename(f.filename)
 		f.save(os.path.join('/home/serj/Desktop/file-hosting/storage', filename))
-		return redirect(url_for('save_file', filename=filename))
+		return redirect(url_for('index', filename=filename, date_time=str(datetime.datetime.now())))
 	else:
 		return render_template('upload_file.html')
 
