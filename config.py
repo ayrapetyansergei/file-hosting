e6cb5554721e9174c44a12703750568b22ac66f5
ayrapetyansergei <ayrapetyan.96@yandex.ru>
2021-08-02 16:22:32 +0300

    edit cofig

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
 import os 
+from dotenv import load_dotenv
+
+
+base_dir = os.getcwd()
+load_dotenv(os.path.join(base_dir, '.env'))
 
 
 class BaseConfig:
 	#App config
-	BASE_DIR = os.getcwd()
-	APP_PORT = os.environ.get('APP_PORT', 8001)
+	APP_PORT = os.environ.get('APP_PORT')
 
 	#MongoDB config
-	MONGO_HOST = os.environ.get('MONGO_HOST', 'mongodb')
-	MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
-	MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'root')
-	MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'password')
+	MONGO_HOST = os.environ.get('MONGO_HOST')
+	MONGO_PORT = os.environ.get('MONGO_PORT')
+	MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
+	MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
 
 class DevelopConfig(BaseConfig):
 	APP_DEBUG = True
